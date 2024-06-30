9600347092d3d91414c0b678dad26e53638a461d
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2024-06-30 17:25:57 +0900

    一旦あきらめ

--- src/gazebo2grid.cpp
+++ src/gazebo2grid.cpp
@@ -16,11 +16,11 @@ Gazebo2Grid::Gazebo2Grid() : nh("~")
 void Gazebo2Grid::initialize_grid()
 {
     // Set the grid size
-    grid.info.width = 100;
-    grid.info.height = 100;
+    grid.info.width = 1000;
+    grid.info.height = 1000;
     grid.info.resolution = 0.1;
-    grid.info.origin.position.x = -5;
-    grid.info.origin.position.y = -5;
+    grid.info.origin.position.x = -50;
+    grid.info.origin.position.y = -50;
     grid.info.origin.position.z = 0;
     grid.info.origin.orientation.x = 0;
     grid.info.origin.orientation.y = 0;
@@ -37,6 +37,7 @@ void Gazebo2Grid::add_object_to_grid(tinyxml2::XMLElement *box, tinyxml2::XMLEle
     const char* sizeText = box->FirstChildElement("size")->GetText();
     std::istringstream sizeStream(sizeText);    //split the string into a string stream
     sizeStream >> size_x >> size_y >> size_z;
+    std::cout << typeid(size_x).name() << std::endl;
     std::cout << "size_x: " << size_x << " size_y: " << size_y << " size_z: " << size_z << std::endl;
     
     // Get the pose of the box
@@ -44,12 +45,18 @@ void Gazebo2Grid::add_object_to_grid(tinyxml2::XMLElement *box, tinyxml2::XMLEle
     const char* poseText = pose->GetText();
     std::istringstream poseStream(poseText);    //split the string into a string stream
     poseStream >> x >> y >> z >> roll >> pitch >> yaw;
+    std::cout << "x: " << x << " y: " << y << " z: " << z << " roll: " << roll << " pitch: " << pitch << " yaw: " << yaw << std::endl;
 
     // Convert the pose to the grid coordinates
     int x_min = (x - size_x / 2 - grid.info.origin.position.x) / grid.info.resolution;
     int x_max = (x + size_x / 2 - grid.info.origin.position.x) / grid.info.resolution;
     int y_min = (y - size_y / 2 - grid.info.origin.position.y) / grid.info.resolution;
     int y_max = (y + size_y / 2 - grid.info.origin.position.y) / grid.info.resolution;
+    std::cout << "x_min: " << x_min << " x_max: " << x_max << " y_min: " << y_min << " y_max: " << y_max << std::endl;
+    if(x_min < 0) x_min = 0;
+    if(x_max > grid.info.width) x_max = grid.info.width;
+    if(y_min < 0) y_min = 0;
+    if(y_max > grid.info.height) y_max = grid.info.height;
 
     // Update the grid
     for (int i = x_min; i < x_max; i++)
@@ -60,7 +67,52 @@ void Gazebo2Grid::add_object_to_grid(tinyxml2::XMLElement *box, tinyxml2::XMLEle
         }
     }
 }
+#if 1
+void Gazebo2Grid::load_world(std::string file_path)
+{
+    //Load the world
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(file_path.c_str());
+    tinyxml2::XMLElement *sdf = doc.FirstChildElement("sdf");
+    tinyxml2::XMLElement *world = sdf->FirstChildElement("world");
+    tinyxml2::XMLElement *model = world->FirstChildElement("model");
 
+    while(model != NULL){
+        const char* model_name = model->Attribute("name");
+        // std::cout << strcmp(model_name, "ground_plane") << std::endl;
+        
+        tinyxml2::XMLElement *link = model->FirstChildElement("link");
+        while(link !=NULL){
+            const char* link_name = link->Attribute("name");
+            // std::cout << "link_name: " << link_name << std::endl;
+            
+            // if(strcmp(link_name, "Wall_85") == 0 || strcmp(link_name, "Wall_86") == 0 || strcmp(link_name, "Wall_83")==0){
+            // if(strcmp(link_name, "Wall_85") == 0){
+                // std::cout << "Wall_85" << std::endl;
+                tinyxml2::XMLElement *visual = link->FirstChildElement("visual");
+                while(visual != NULL){
+                    tinyxml2::XMLElement *geometry = visual->FirstChildElement("geometry");
+                    tinyxml2::XMLElement *pose = link->FirstChildElement("pose");
+                    tinyxml2::XMLElement *box = geometry->FirstChildElement("box");
+                    if(box != NULL){
+                        add_object_to_grid(box, pose);
+                    }
+                    visual = visual->NextSiblingElement("visual");
+                }
+            // }
+            link = link->NextSiblingElement("link");
+        }
+
+
+        model = model->NextSiblingElement("model");
+    }
+    grid.header.frame_id = "map";
+    grid.header.stamp = ros::Time::now();
+    pub_grid.publish(grid);
+}
+#endif
+
+#if 0
 void Gazebo2Grid::load_world(std::string file_path)
 {
     // Load the world
@@ -70,34 +122,59 @@ void Gazebo2Grid::load_world(std::string file_path)
     tinyxml2::XMLElement *world = sdf->FirstChildElement("world");
     tinyxml2::XMLElement *model = world->FirstChildElement("model");
 
+
     while (model != NULL)
     {
-        if(model->Attribute("name")=="ground_plane") continue;
+        const char* model_name = model->Attribute("name");
+        if(strcmp(model_name, "ground_plane") == 0) std::cout << "ground_plane" << std::endl;
         else{
-            tinyxml2::XMLElement *link = model->FirstChildElement("link");
-            while(link != NULL){
-                tinyxml2::XMLElement *visual = link->FirstChildElement("visual");
-                while(visual != NULL){
-                    tinyxml2::XMLElement *geometry = visual->FirstChildElement("geometry");
-                    tinyxml2::XMLElement *pose = visual->FirstChildElement("pose");
-                    tinyxml2::XMLElement *box = geometry->FirstChildElement("box");
-                    if(box != NULL){
-                        add_object_to_grid(box, pose);
-                    }
-                    visual = visual->NextSiblingElement("visual");
-                }
+            std::cout << "model_name: " << model_name << std::endl;
+            // tinyxml2::XMLElement *link = model->FirstChildElement("link");
+
+            // while(link != NULL){
+            //     const char* link_name = link->Attribute("name");
+                
+            //     //test
+            //     std::cout << typeid(link_name).name() << std::endl;
+            //     std::cout << typeid("Wall_85").name() << std::endl;
+
+            //     if(strcmp(link_name, "Wall_85") == 0){
+            //         std::cout << "Wall_85" << std::endl;
+            //         std::cout << "link->Attribute(name): " << link->Attribute("name") << std::endl;
+            //         tinyxml2::XMLElement *visual = link->FirstChildElement("visual");
+            //         while(visual != NULL){
+            //             tinyxml2::XMLElement *geometry = visual->FirstChildElement("geometry");
+            //             tinyxml2::XMLElement *pose = visual->FirstChildElement("pose");
+            //             tinyxml2::XMLElement *box = geometry->FirstChildElement("box");
+            //             if(box != NULL){
+            //                 add_object_to_grid(box, pose);
+            //             }
+            //             visual = visual->NextSiblingElement("visual");
+            //         }
+            //     }
+
+                // tinyxml2::XMLElement *visual = link->FirstChildElement("visual");
+                // while(visual != NULL){
+                //     tinyxml2::XMLElement *geometry = visual->FirstChildElement("geometry");
+                //     tinyxml2::XMLElement *pose = visual->FirstChildElement("pose");
+                //     tinyxml2::XMLElement *box = geometry->FirstChildElement("box");
+                //     if(box != NULL){
+                //         add_object_to_grid(box, pose);
+                //     }
+                //     visual = visual->NextSiblingElement("visual");
+                // }
                 link = link->NextSiblingElement("link");
             }
 
         }
 
         model = model->NextSiblingElement("model");
-    }
+        }
     grid.header.frame_id = "map";
     grid.header.stamp = ros::Time::now();
     pub_grid.publish(grid);
 }
-
+#endif
 void Gazebo2Grid::run()
 {
     // Publish the grid
